4da172bf0b1d341a4220d66163107abc92351d61
carlos uriel <42219332+Carlosuriel1999@users.noreply.github.com>
2018-10-10 16:09:18 -0500

    Add files via upload

---
+++ mapitas/mapitas/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  mapitas
+//
+//  Created by Macbook on 10/10/18.
+//  Copyright © 2018 ServiciosUHB. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapa: MKMapView!
+    let locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapa.delegate = self  // self es toda la clase y le damos los delegados para que implemente la clase
+        locationManager.delegate = self
+        
+        locationManager.requestAlwaysAuthorization() //desired acurracy es para que presiso deseas que sea la ubica
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        locationManager.startUpdatingLocation()
+        mapa.showsUserLocation = true
+    }
+
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let region = MKCoordinateRegion(center: mapa.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009))
+        
+        mapa.setRegion(region, animated: true)
+        
+        
+    }
+
+}
+
